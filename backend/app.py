46917f4f401501b35b2455190b4a353232aa74a7
IshmamF <ishmamf2003@gmail.com>
2024-09-14 17:03:46 -0400

    openai vision to read text from multiple images

---
+++ backend/app.py
@@ -0,0 +1,61 @@
+import base64
+import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+OPEN_AI_KEY = os.getenv('OPEN_AI_KEY')
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+image_lists = []
+
+for i in range(1,4):
+    base64_image = encode_image(f"./images/img{i}.webp")
+    image_lists.append(base64_image)
+
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": f"Bearer {OPEN_AI_KEY}"
+}
+
+payload = {
+  "model": "gpt-4o-mini",
+  "messages": [
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "What’s in this image?"
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{image_lists[0]}"
+          }
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{image_lists[1]}"
+          }
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{image_lists[2]}"
+          }
+        }
+      ]
+    }
+  ],
+  "max_tokens": 300
+}
+
+response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+print(response.json())
---
+++ backend/docs.md
@@ -0,0 +1,6 @@
+# Get Started
+Create virtual environment: `python -m venv <insert name>`
+Go into virtual environment: `source <name>/bin/activate`
+Install: `pip install -r requirements.txt`
+
+
